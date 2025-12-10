d019f4d22df2d5939b1b8db761f5297d5150346a
Jonas <joda12@elev.techcollege.dk>
2025-12-10 12:06:55 +0100

    Initial commit

---
+++ my-api/src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import { userRoutes } from './routes/userRoutes.js'
+
+dotenv.config({ quiet: true })
+
+const port = process.env.PORT || 3000
+
+const app = express()
+
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/api/users', userRoutes)
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+    
+})
